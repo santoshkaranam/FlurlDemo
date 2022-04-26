9be36230ba7263658d6e789a35b2f770a640c7fb
sankaranam <karanam@iiasa.ac.at>
2022-04-26 08:40:46 +0200

    more usages examples

--- FlurlDemo/Program.cs
+++ FlurlDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using Flurl.Http;
 using RestSharp;
@@ -7,6 +8,7 @@ namespace FlurlDemo
 {
     class Program
     {
+        // reading weather data from open weather map. If the below keys dont work, please register new API key and use them.
         private const string BaseUrl = "https://community-open-weather-map.p.rapidapi.com";
         private const string ResourcePath =
             "/forecast/daily?q=bengaluru%2CIN&lat=35&lon=139&cnt=10&units=metric%20or%20imperial";
@@ -31,8 +33,18 @@ namespace FlurlDemo
             Console.WriteLine("-----------------------Flurl Demo---------------------");
             Console.WriteLine();
             var url = BaseUrl + ResourcePath;
+
             Console.WriteLine(url.WithHeader(XRapidapiHost, XRapidapiHostValue)
                 .WithHeader(XRapidapiKey, XRapidapiKeyValue).GetStringAsync().Result);
+
+            
+            //Supports Builder Pattern, simple and effective way to handle
+            //url.WithTimeout(100); with timeout
+            //url.WithBasicAuth("username", "******");
+            //url.AllowHttpStatus(HttpStatusCode.NotFound); allows to add success codes in addition to 2XX, so that exception is not thrown.
+            //url.WithOAuthBearerToken("myBearerToken");
+            //url.PostJsonAsync() directly POST json object with application/json, same way also for PUT
+            //url.GetStreamAsync() get stream and save to file or pass down the stream in web api response.
 
             Console.Read();
         }
